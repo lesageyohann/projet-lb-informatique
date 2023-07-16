61f65d0576861264c1d147decc6ce831a6f5fad0
yohann <lesage.ly@gmail.com>
2023-07-16 14:29:49 +0200

    update partenair

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,26 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react';
+import P1 from '../Assets/logo-nouvelle-aquitaine.png'
 
 function Footer() {
 
-    const [activeTab, setActiveTab] = useState('Accueil')
-
-    const tabClick = (tab) => {
-        setActiveTab(tab)}
 
     return (
        <footer className='footer'>
-        <ul className='footer_partener'>
-            <li>
-                
-            </li>
-        </ul>
-        <ul className='footer_nav'>
-            <li className='footer_nav_bar'>
-            <Link className= {`footer_nav_bar_button`} onClick={() => tabClick('Accueil')} to="/">Accueil</Link>
-            <Link className= {`footer_nav_bar_button`} onClick={() => tabClick('Contact')} to="/Contact">Contact</Link>
-            <Link className= {`footer_nav_bar_button`} onClick={() => tabClick('ML')} to="/ML">Mentions légales</Link>
-            </li>
-        </ul>
+        <div className='footer_partenair'>
+            <div className='footer_partenair_title'>Ils nous font confiance :</div>
+            <div className='footer_partenair_divimg'><a href='https://lesirque.com'><img className='footer_partenair_divimg_img' src={P1} alt='logo partenaire'/></a></div>
+            <div className='footer_partenair_divimg'></div>
+        </div>
+        <div className='footer_link'>
+            <ul className='footer_link_nav'>
+                <li className='footer_link_nav_bar'>
+                <Link className= 'footer_link_nav_bar_button' to="/">Accueil</Link>
+                <Link className= 'footer_link_nav_bar_button' to="/Contact">Contact</Link>
+                <Link className= 'footer_link_nav_bar_button' to="/ML">Mentions légales</Link>
+                </li>
+            </ul>
+        </div>
+        
        </footer>
 
     )
